6c73e3a708921556b8975467d7470fb60e1b3670
Michal901 <momot1122@wp.pl>
2024-06-06 12:42:41 +0200

    .

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,43 +1,54 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-import styles from './ContactForm.module.css';
+import { useDispatch } from 'react-redux';
+import { addContact } from '../../redux/contacts/operations';
 
-const ContactForm = ({ addContact }) => {
+const ContactForm = () => {
   const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
+  const [number, setNumber] = useState('');
+  const dispatch = useDispatch();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    addContact({ name, phone });
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    // Log the data before sending
+    console.log({ name, number });
+
+    if (!name || !number) {
+      console.error('Missing required fields');
+      return;
+    }
+
+    dispatch(addContact({ name, number }));
+
     setName('');
-    setPhone('');
+    setNumber('');
   };
 
   return (
-    <form className={styles.contactForm} onSubmit={handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input
-        type="text"
-        id="name"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        required
-      />
-      <label htmlFor="phone">Phone</label>
-      <input
-        type="tel"
-        id="phone"
-        value={phone}
-        onChange={e => setPhone(e.target.value)}
-        required
-      />
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={name}
+          onChange={e => setName(e.target.value)}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Phone:
+        <input
+          type="tel"
+          value={number}
+          onChange={e => setNumber(e.target.value)}
+          required
+        />
+      </label>
+      <br />
       <button type="submit">Add Contact</button>
     </form>
   );
-};
-
-ContactForm.propTypes = {
-  addContact: PropTypes.func.isRequired,
 };
 
 export default ContactForm;
